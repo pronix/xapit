a0bc77eb34c1669d2efb9194caf843a310b62afa
dima <pronix.service@gmail.com>
2010-03-10 02:50:44 +0300

    fast fix for ruby19

--- install.rb
+++ install.rb
@@ -1,5 +1,3 @@
-require 'ftools'
-
 source = File.join(File.dirname(__FILE__), '/rails_generators/xapit/templates/setup_xapit.rb')
 destination = "#{Rails.root}/config/initializers/setup_xapit.rb"
 unless File.exist? destination
